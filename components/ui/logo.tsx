1ccea4236fdba09c9db693c8ee5d40644865195b
ismael.rumzan <ismael@cordobadigitalmedia.com>
2024-06-24 23:54:26 -0600

    add title

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,17 +3,20 @@ import Link from "next/link";
 
 interface LogoProps {
   iconOnly?: boolean;
+  title?: string;
 }
 
-export function Logo({ iconOnly = false }: LogoProps) {
+export function Logo({ iconOnly = false, title }: LogoProps) {
   return (
     <Link
-      className="flex rounded-xl text-xl font-semibold focus:opacity-80 focus:outline-none"
+      className="flex rounded-xl text-xl font-semibold text-gray-800 focus:opacity-80 focus:outline-none dark:text-gray-200"
       href="/"
       aria-label="Preline"
     >
-      <Image alt="Site logo" height="24" src="/favicon.png" width="24" />
-      {!iconOnly && <span>LMS</span>}
+      {!title && (
+        <Image alt="Site logo" height="24" src="/favicon.png" width="24" />
+      )}
+      {!iconOnly && <span>{title ? title : `Learning Management System`}</span>}
     </Link>
   );
 }
